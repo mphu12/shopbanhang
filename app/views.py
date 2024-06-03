4b56fc0a3c113aeb36319ff10b16d52734dd38fe
mphu12 <minhphu7902@gmail.com>
2024-06-03 16:05:20 +0700

    int project

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    context= {}
+    return render(request,'app/home.html',context)
+
+def cart(request):
+    context= {}
+    return render(request, 'app/cart.html',context)
+
+def checkout(request):
+    context= {}
+    return render(request, 'app/checkout.html',context)
